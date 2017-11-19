3a88acb096d3ada942c5c3213a17412b1f61606a
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2017-11-19 03:30:19 -0800

    Added files to main gem file

--- lib/fried/typings.rb
+++ lib/fried/typings.rb
@@ -1,9 +1,12 @@
 require "fried/typings/version"
-require "fried/typings/type"
-require "fried/typings/meta_type"
-require "fried/typings/is"
-require "fried/typings/one_of"
 require "fried/typings/boolean"
+require "fried/typings/is"
+require "fried/typings/is_strictly"
+require "fried/typings/meta_type"
+require "fried/typings/one_of"
+require "fried/typings/strictly_one_of"
+require "fried/typings/tuple_of"
+require "fried/typings/type"
 
 module Fried
   module Typings
